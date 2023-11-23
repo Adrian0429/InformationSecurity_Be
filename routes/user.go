2c08fbda8dc61d0a7f0b63ae762983ca0f2fe967
Adrian Karuna Soetikno <adrian@Adrians-MacBook-Pro.local>
2023-11-23 16:20:00 +0700

    handle CORS

--- routes/user.go
+++ routes/user.go
@@ -18,12 +18,13 @@ func User(route *gin.Engine, userController controller.UserController, jwtServic
 		routes.PATCH("/", middleware.Authenticate(jwtService), userController.UpdateUser)
 		routes.GET("/me", middleware.Authenticate(jwtService), userController.MeUser)
 		routes.POST("/upload/:method", middleware.Authenticate(jwtService), userController.Upload)
-		routes.GET("/get/:path/:ownerid/:id/:method/", middleware.Authenticate(jwtService), userController.GetMedia)
-		routes.GET("/getbykey/:path/:ownerid/:id/:method/", middleware.Authenticate(jwtService), userController.GetMediaWithKey)
+		routes.GET("/get/:path/:ownerid/:id/:method", middleware.Authenticate(jwtService), userController.GetMedia)
+		routes.GET("/getbykey/:path/:ownerid/:id/:method", middleware.Authenticate(jwtService), userController.GetMediaWithKey)
 		routes.GET("/get/:path/KTP/:ownerid", middleware.Authenticate(jwtService), userController.GetKTP)
 		routes.POST("/send/Request/:ownerid", middleware.Authenticate(jwtService), userController.SendRequest)
 		routes.POST("/send/Acceptance/:requestid", middleware.Authenticate(jwtService), userController.SendAcceptanceEmail)
 		routes.GET("/getAllMedia", middleware.Authenticate(jwtService), userController.GetAllMedia)
+
 		// Admin
 		routes.PATCH("/verify", middleware.Authenticate(jwtService), userController.UpdateStatusIsVerified)
 	}
